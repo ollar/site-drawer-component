2dfa5a8e6b55f200e18160eda166c7ab4b3b3852
Oleg Larkin <oleg.larkin@nordigy.ru>
2018-06-28 16:01:48 +0300

    drawer dev

--- addon/components/site-drawer-aside.js
+++ addon/components/site-drawer-aside.js
@@ -2,5 +2,13 @@ import Component from '@ember/component';
 import layout from '../templates/components/site-drawer-aside';
 
 export default Component.extend({
-  layout
+  layout,
+  tagName: 'aside',
+  classNames: ['drawer'],
+
+  actions: {
+    toggleDrawer() {
+      if (this.toggleDrawer && this.toggleDrawer.call) this.toggleDrawer();
+    },
+  },
 });
