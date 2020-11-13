8454761309e96429ee3fcc1003ff63a39833a484
Oleg Larkin <olegollar@gmail.com>
2020-11-13 19:18:31 +0300

    drawer rewritten

--- addon/components/site-drawer.js
+++ addon/components/site-drawer.js
@@ -1,47 +1,37 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { schedule } from '@ember/runloop';
 import { action } from '@ember/object';
 import { tracked } from "@glimmer/tracking";
 
 export default class SiteDrawerComponent extends Component {
-  @service router;
-
   @tracked opened = false;
 
-  constructor() {
-      super(...arguments);
-
-      this.router.addObserver('currentRouteName', this, 'closeDrawer');
-  }
+  asideComponent = 'site-drawer-aside';
+  overlayComponent = 'site-drawer-overlay';
+  triggerComponent = 'site-drawer-trigger';
 
   _openDrawer() {
-    schedule('afterRender', () => {
       this.opened = true;
-    });
   }
 
   _closeDrawer() {
-    schedule('afterRender', () => {
       this.opened = false;
-    });
   }
 
   @action
   toggleDrawer(event) {
-    event.preventDefault && event.preventDefault();
+    event && event?.preventDefault();
     this.opened = !this.opened;
   }
 
   @action
   openDrawer(event) {
-    event.preventDefault && event.preventDefault();
+    event && event?.preventDefault();
     this._openDrawer();
   }
 
   @action
   closeDrawer(event) {
-    event.preventDefault && event.preventDefault();
+    event && event?.preventDefault();
     this._closeDrawer();
   }
 }
--- tests/dummy/app/components/site-drawer-aside.js
+++ tests/dummy/app/components/site-drawer-aside.js
@@ -1,9 +1,3 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
-export default class SiteDrawerAsideComponent extends Component {
-    @action
-    closeDrawer() {
-        if (this.closeDrawer && this.closeDrawer.call) this.args.closeDrawer(...arguments);
-    }
-}
+export default class SiteDrawerAsideComponent extends Component {}
